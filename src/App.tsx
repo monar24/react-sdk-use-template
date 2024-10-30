34c6e9c0d3c61ea5ab9e77472f9111a4f2dc3ea5
monar24 <mona@oneof.com>
2024-10-30 11:03:46 -0400

    push imported sdk

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { SDK } from 'sdktest';
 
-function App() {
+const App: React.FC = () => {
+  useEffect(() => {
+    const sdk = new SDK({ apiKey: 'my-secret-api-key', environment: 'production' });
+
+    sdk.getConfig();        
+    sdk.greetUser("User#001"); 
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello from React App</h1>
       </header>
     </div>
   );
-}
+};
 
 export default App;
