5b9d3530e10a323da97c26facca58fd876c87694
monar24 <mona@oneof.com>
2024-11-08 12:23:09 -0500

    sdk related code

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import React, { useEffect } from 'react';
-import { SDK } from 'sdktest';
+import { PaymentSDK } from 'sdktest';
 
 const App: React.FC = () => {
-  useEffect(() => {
-    const sdk = new SDK({ apiKey: 'my-secret-api-key', environment: 'production' });
 
-    sdk.getConfig();        
-    sdk.greetUser("User#001"); 
+  useEffect(() => {
+    // Define an async function within the useEffect hook
+    const fetchUSDValue = async () => {
+      const sdk = new PaymentSDK({
+        apiKey: '4dC35FVfY53mWa7vYZScs1K0FVyVcAK31D0zbWCM',
+        environment: 'production',  // Or 'sandbox'
+      });
+
+      sdk.getConfig();
+
+      try {
+        const quote = await sdk.getUSDValue("SOL", 1);
+        console.log('QUOTE IS: ', quote);
+      } catch (error) {
+        console.error("Error fetching USD value:", error);
+      }
+    };
+
+    fetchUSDValue();
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello from React App</h1>
+        <h1>Testing App</h1>
       </header>
     </div>
   );
